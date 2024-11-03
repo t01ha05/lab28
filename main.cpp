6957c520de61ef1fc3eb54cae2ecb90fbd02d008
Talha Ahmed <t01haa2005@gmail.com>
2024-11-02 20:11:22 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>
+#include <algorithm>
 #include <list>
 #include "Goat.h"
 using namespace std;
@@ -12,6 +12,14 @@ void delete_goat(list<Goat> &trip);
 void add_goat(list<Goat> &trip, string [], string []);
 void display_trip(list<Goat> trip);
 int main_menu();
+void find_goat_by_name(list<Goat> goats);
+void count_goats_by_color(list<Goat> goats);
+void display_goats_reversed(list<Goat>& goats);
+void shuffle_goats(list<Goat> goats);
+void remove_specific_goat();
+void replace_goat_color();
+void sort_goats_by_age(list<Goat> goats);
+void calculate_average_age();
 
 int main() {
     srand(time(0));
@@ -44,7 +52,7 @@ int main() {
     
     // Goat Manager 3001 Engine
     int sel = main_menu();
-    while (sel != 4) {
+    while (sel != 12) {
         switch (sel) {
             case 1:
                 cout << "Adding a goat.\n";
@@ -57,6 +65,14 @@ int main() {
             case 3:    
                 cout << "Displaying goat data.\n";
                 display_trip(trip);
+                break;
+            case 4:
+                cout << "Finding goat by name.\n";
+                find_goat_by_name(trip);
+                break;
+            case 5:
+                cout << "Counting goats by color.\n";
+                count_goats_by_color(trip);
                 break;
             default:
                 cout << "Invalid selection.\n";
@@ -74,11 +90,19 @@ int main_menu() {
     cout << "[1] Add a goat\n";
     cout << "[2] Delete a goat\n";
     cout << "[3] List goats\n";
-    cout << "[4] Quit\n";
+    cout << "[4] Find goat by name\n";
+    cout << "[5] Count goats by color\n";
+    cout << "[6] List all goats in reverse order\n";
+    cout << "[7] Shuffle goats\n";
+    cout << "[8] Remove a specific goat\n";
+    cout << "[9] Replace goat color\n";
+    cout << "[10] Sort goats by age and list\n";
+    cout << "[11] Calculate average age of goats\n";
+    cout << "[12] Quit\n";
     cout << "Choice --> ";
     int choice;
     cin >> choice;
-    while (choice < 1 || choice > 4) {
+    while (choice < 1 || choice > 12) {
         cout << "Invalid, again --> ";
         cin >> choice;
     }
@@ -125,4 +149,39 @@ int select_goat(list<Goat> trp) {
         cin >> input;
     }
     return input;
+}
+
+void find_goat_by_name(list<Goat> goats) {
+    string name;
+    cout << "Enter name to find: ";
+    cin >> name;
+    for (Goat g : goats) {
+        if (g.get_name() == name)
+            cout << "Found: " << g.get_name() << endl;
+    }
+}
+
+void count_goats_by_color(list<Goat> goats) {
+    string color;
+    cout << "Enter color to count: ";
+    cin >> color;
+    int count = 0;
+    for (Goat g : goats) {
+        if (g.get_color() == color)
+            count++;
+    }
+    cout << "Found " << count << " goats" << endl;
+}
+
+void display_goats_reversed(list<Goat>& goats) {
+    for (int i = goats.size() - 1; i >= 0; i--)
+        cout << goats[i] << endl;
+}
+
+void shuffle_goats(list<Goat> goats) {
+    random_shuffle(goats.begin(), goats.end());
+}
+
+void sort_goats_by_age(list<Goat> goats) {
+    sort(goats.begin(), goats.end());
 }
